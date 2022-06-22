b871916d856d5d1c1790a5ab1ded3dbc765d9365
TemaTerbi <tematerbi@mail.ru>
2022-06-22 15:30:33 +0300

    Initial Commit

---
+++ RIck-and-Morty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RIck-and-Morty
+//
+//  Created by deniss den on 22.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RIck-and-Morty/RIck_and_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RIck_and_MortyApp.swift
+//  RIck-and-Morty
+//
+//  Created by deniss den on 22.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct RIck_and_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
